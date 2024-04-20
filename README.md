e54624d238300f31547b580d37341ad3ac7e4864
Nathaniel A Collier <drxs3v3n@gmail.com>
2024-04-20 11:14:40 -0500

    move notes to the readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # repl_rust
  Misc programs written in rust
+
+
+## Install rust on arch
+```bash
+# Install rustup
+sudo pacman -S rustup
+# setup rustup to stable which will download all the rust tools including rustc to compile
+rustup default stable
+# make sure rustc works
+rustc -version
+```
+
--- test/singleFiles/test001_helloWorld.rs
+++ test/singleFiles/test001_helloWorld.rs
@@ -3,15 +3,7 @@
 compile with
 - rustc test001_helloWorld.rs
 
-# Install rust on arch
-```
-# Install rustup
-sudo pacman -S rustup
-# setup rustup to stable which will download all the rust tools including rustc to compile
-rustup default stable
-# make sure rustc works
-rustc -version
-```
+
 */
 
 fn main(){
